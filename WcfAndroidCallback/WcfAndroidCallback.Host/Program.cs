24bbdd139ec2bfd4a301906ddf1fba9f14b2d8cb
shichaohu <502242999@qq.com>
2017-08-20 23:32:36 +0800

    1

---
+++ WcfAndroidCallback/WcfAndroidCallback.Host/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using WcfAndroidCallback.Service;
+
+namespace WcfAndroidCallback.Host
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ServiceHost host = new ServiceHost(typeof(AndroidService)))
+            {
+                host.Opened += (sender, e) => Console.WriteLine("服务开启..");
+                host.Open();
+
+                Console.WriteLine("输入quit退出");
+                string input = Console.ReadLine();
+                while (input.ToLower() != "quit")
+                {
+                    input = Console.ReadLine();
+                }
+            }
+        }
+    }
+}
