8cfe3057e1b9a022f87b39151bef3f50120ca8f6
shichaohu <502242999@qq.com>
2017-08-21 10:09:04 +0800

    架构调整

---
+++ Responder.Wcf/Responder.Wcf.Service.Interface/IAndroidService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace Responder.Wcf.Service.Interface
+{
+    [ServiceContract(CallbackContract = typeof(IAndroidCallBack))]
+    public interface IAndroidService
+    {
+        [OperationContract]
+        string PrintString(string value);
+
+        [OperationContract]
+        void NeedCallBack();
+
+    }
+
+    public interface IAndroidCallBack
+    {
+        [OperationContract(IsOneWay = true)]
+        void Show(string backMessage);
+    }
+}
---
+++ Responder.Wcf/Responder.Wcf.Service/AndroidService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using Responder.Wcf.Service.Interface;
+
+namespace Responder.Wcf.Service
+{
+    public class AndroidService : IAndroidService
+    {
+        public string PrintString(string value)
+        {
+            return "your string:" + value;
+        }
+
+        public void NeedCallBack()
+        {
+            var callBackChannel = OperationContext.Current.GetCallbackChannel<IAndroidCallBack>();
+
+            string backMessage = "这是回调消息！";
+            callBackChannel.Show(backMessage);
+        }
+    }
+}
